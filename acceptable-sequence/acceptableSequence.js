58123be825c8377d9b04a693937eb04635b44d46
Romeo <romeocorpuz808@gmail.com>
2018-06-07 16:31:42 -1000

    submit

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,29 @@
 module.exports = function acceptableSequence(str) {
-  // write your code here
+  
+  var Letter = str.match(/[a-z]/gi); // Strings have indexes!!!
+  console.log(Letter);
+  
+  var marked = true;  //Replace return
+
+  //Loop array of letters and check for '+' characters before and after 
+  for(var i=0; i<Letter.length; i++){  
+
+    var dir = str.indexOf(Letter[i]);  //Checks index of the letter
+
+    var before = dir - 1; // index before letter
+    var after = dir + 1; // index after letter
+
+    //If before and after the letter has '+'s, then marked = true 
+    if(str[before] === "+" && str[after] === "+"){
+      marked = true;
+    }
+    // If before or after letter is not a '+'s, then marked = false. Per TA's return stops the codes.
+    else if(str[before] !== "+" || str[after] !== "+"){
+      marked = false;
+      console.log("Unacceptable");
+      return marked;
+    }
+    console.log("Acceptable");
+    return marked;
+  }
 }
