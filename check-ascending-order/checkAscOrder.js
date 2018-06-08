f59d1450e91f68d804b5dd520017e29d594834d0
Romeo <romeocorpuz808@gmail.com>
2018-06-07 17:49:35 -1000

    submit

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,30 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+
+  var first;
+
+  var second;
+
+  var count = 0;
+
+  for(var i = 0; i<numArray.length; i++){
+    count++;
+
+    first = numArray[i];//Set first and second numbers
+    
+    if(count !== numArray.length){//Stop and return on the last number.
+      second = numArray[i+1];
+    }
+    else
+      return flag;
+
+    if(first < second){//If first is smaller, ascendi order and return true
+      flag = true;
+    }  
+    else{//if first is bigger, return false
+      flag = false;
+      return flag;
+    }
+  }
+  return flag;
 }
